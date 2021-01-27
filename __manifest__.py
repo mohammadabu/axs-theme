ae04e66d83100be7787fcf64a689b46dda477b0d
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-01-27 13:33:31 +0200

    EDIT

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'AXS Theme',
+    'version': '12.0.1.0.0',
+    'summary': 'AXS Theme',
+    'company': 'AXS',
+    'depends': [
+        'web'
+    ],
+    'description': """
+           AXS Theme
+    """,
+   'data': [
+    'views/webclient_template_extend.xml',
+    ],
+}
